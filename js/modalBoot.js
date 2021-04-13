47d90ec616c7a51052ba607d4d5c3ff6a60d5d73
Omari Jalagania <omari.jalagania@gmail.com>
2021-04-13 19:57:41 +0400

    fix one time popup

--- js/modalBoot.js
+++ js/modalBoot.js
@@ -3,25 +3,33 @@ $(document).ready(function() {
         const $popup = $('.video-popup');
         const $modal = $('#modal');
         const $closeIcon = $('.close');
+        const dialogShow = $.cookie('dialogShow');
+
+
+        if(!dialogShow){
+            $(document).ready(function() {
+                $popup.fadeIn(200);
+                $modal.fadeIn(200);
+                $.cookie('dialogShow', 1);
+               
+            });
+            $closeIcon.click(function () {
+                $popup.fadeOut(200);
+                $modal.fadeOut(200);
+                $(".video-player--you").first().attr('src','');
+            });
+            // for escape key
+            $(document).on('keyup',function(e) {
+                if (e.key === "Escape") {
+                   $popup.fadeOut(200);
+                   $modal.fadeOut(200);
+                   $(".video-player--you").first().attr('src','');
+                }
+            });
+        }else {
+            $popup.fadeOut(200)
+        }
         
-        $(document).ready(function() {
-            $popup.fadeIn(200);
-            $modal.fadeIn(200);
-           
-        });
-        $closeIcon.click(function () {
-            $popup.fadeOut(200);
-            $modal.fadeOut(200);
-            $(".video-player--you").first().attr('src','');
-        });
-        // for escape key
-        $(document).on('keyup',function(e) {
-            if (e.key === "Escape") {
-               $popup.fadeOut(200);
-               $modal.fadeOut(200);
-               $(".video-player--you").first().attr('src','');
-            }
-        });
         // click outside of the popup, close it
         $modal.on('click', function(e){
             $popup.fadeOut(200);
